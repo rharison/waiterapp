5caba2f55bf96e744f5c21aa86e8f2cf6b230313
@rharison <rharison.abreu@gmail.com>
2022-11-15 20:12:47 -0300

    feat: create app component

---
+++ fe/src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+import { Header } from './components/Header';
+
+export function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Header/>
+    </>
+  );
+}

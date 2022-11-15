2814205c77c32aea16e2ab1f10a58b274a8c3502
@rharison <rharison.abreu@gmail.com>
2022-11-15 11:18:03 -0300

    feat: first version to index

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,18 @@
-let texto: String;
+import * as dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+dotenv.config();
 
-texto = "Hola Mundo";
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.odl9svw.mongodb.net/?retryWrites=true&w=majority`)
+  .then(() => {
+    const app = express();
+    const port = process.env.PORT || 3001;
 
-console.log(texto);
+    app.listen(port, () => {
+      console.log(`🚀 Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => {
+    console.log('Connection failed to MongoDB.');
+  });
+

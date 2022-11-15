efdbf5e849e39534f4fee4b035fad5f1f3ed7f66
@rharison <rharison.abreu@gmail.com>
2022-11-15 13:27:58 -0300

    feat: create use case to create product

---
+++ api/src/app/useCases/products/createProduct.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { Product } from '../../models/Product';
+
+export async function createProduct(req: Request, res: Response) {
+  try {
+    const imagePath = req.file?.filename;
+    const { name, description, price, igredients, category } = req.body;
+
+    const product = await Product.create({
+      name,
+      description,
+      imagePath,
+      price: Number(price),
+      igredients: JSON.parse(igredients),
+      category,
+    });
+
+    res.status(201).json(product);
+  } catch(err) {
+    console.log('❌ - Error in create product: ', err);
+    res.sendStatus(500);
+  }
+}

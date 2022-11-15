0a4efa6ac11b0d9b3eccbc44cab13647963fdb16
@rharison <rharison.abreu@gmail.com>
2022-11-15 11:53:25 -0300

    feat: add router in app express

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import { router } from './router';
 dotenv.config();
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.odl9svw.mongodb.net/?retryWrites=true&w=majority`)
@@ -8,6 +9,7 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS
     const app = express();
     const port = process.env.PORT || 3001;
 
+    app.use(router);
     app.listen(port, () => {
       console.log(`🚀 Server is running on http://localhost:${port}`);
     });

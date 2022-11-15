a871e93c8c8357d3c15fdd9e139fffd7c6c5accc
@rharison <rharison.abreu@gmail.com>
2022-11-15 14:19:03 -0300

    feat: create use case list orders

---
+++ api/src/app/useCases/orders/listOrders.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { Order } from '../../models/Order';
+
+export async function listOrders(req: Request, res: Response) {
+  try {
+    const orders = await Order.find()
+      .sort({ createdAt: 1 })
+      .populate('products.product');
+
+    res.json(orders);
+  } catch (err) {
+    console.log('❌ - Error in list orders: ', err);
+    res.sendStatus(500);
+  }
+
+}

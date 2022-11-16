3d30a7e99f9134590f9aead2defe3fb18ce10619
@rharison <rharison.abreu@gmail.com>
2022-11-16 20:17:24 -0300

    feat: init create app mobile

---
+++ app/src/Main/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+import { Header } from '../components/Header';
+
+export function Main() {
+  return (
+    <Container>
+      <Header/>
+    </Container>
+  );
+}

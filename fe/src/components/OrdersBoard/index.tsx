caaf8c62ac5532274bfc3f2adc2e877a96aa26d4
@rharison <rharison.abreu@gmail.com>
2022-11-18 07:57:19 -0300

    feat: add control open and close modal

--- fe/src/components/OrdersBoard/index.tsx
+++ fe/src/components/OrdersBoard/index.tsx
@@ -11,14 +11,25 @@ interface OrdersBoardProps {
 
 export function OrdersBoard({ icon, title, orders } : OrdersBoardProps) {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState<null | Order>(null);
 
-  function handleOpenModal() {
+  function handleOpenModal(order: Order) {
     setIsModalVisible(true);
+    setSelectedOrder(order);
+  }
+
+  function handleCloseModal() {
+    setIsModalVisible(false);
+    setSelectedOrder(null);
   }
 
   return (
     <Board>
-      <OrderModal visible={isModalVisible}/>
+      <OrderModal
+        visible={isModalVisible}
+        order={selectedOrder}
+        onClose={handleCloseModal}
+      />
       <header>
         <span>{icon}</span>
         <strong>{title}</strong>
@@ -28,7 +39,7 @@ export function OrdersBoard({ icon, title, orders } : OrdersBoardProps) {
       {orders.length > 0 &&
         <OrdersContainer>
           {orders.map(order => (
-            <button type='button' key={order._id} onClick={handleOpenModal}>
+            <button type='button' key={order._id} onClick={() => handleOpenModal(order)}>
               <strong>Mesa {order.table}</strong>
               <span>{order.products.length} itens</span>
             </button>

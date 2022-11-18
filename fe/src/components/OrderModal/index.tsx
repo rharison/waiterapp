f6717bbc90b1bebdf929b101b663f65a5e528aa9
@rharison <rharison.abreu@gmail.com>
2022-11-18 07:57:47 -0300

    feat: create a modal view order and add styles

--- fe/src/components/OrderModal/index.tsx
+++ fe/src/components/OrderModal/index.tsx
@@ -1,21 +1,45 @@
-import { Overlay, ModalBody } from './styles';
+import { Overlay, ModalBody, OrderDetails, Actions } from './styles';
 import closeIcon from '../../assets/images/close-icon.svg';
+import { Order } from '../../types/Order';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { useEffect } from 'react';
 
 interface OrderModalProps {
   visible: boolean;
+  order: Order | null;
+  onClose: () => void;
 }
 
-export function OrderModal({ visible }: OrderModalProps) {
-  if(!visible) {
+export function OrderModal({ visible, order, onClose }: OrderModalProps) {
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+
+  }, [onClose]);
+
+  if(!visible || !order) {
     return null;
   }
+
+  const total = order.products.reduce((total, { product, quantity }) => {
+    return total + (product.price * quantity);
+  }, 0);
 
   return (
     <Overlay>
       <ModalBody>
         <header>
-          <strong>Mesa 2</strong>
-          <button type="button">
+          <strong>Mesa {order.table}</strong>
+          <button type="button" onClick={onClose}>
             <img src={closeIcon} alt="Ícone de fechar" />
           </button>
         </header>
@@ -23,10 +47,58 @@ export function OrderModal({ visible }: OrderModalProps) {
         <div className="status-container">
           <small>Status do pedido</small>
           <div>
-            <span>🕧</span>
-            <strong>Fila de espera</strong>
+            <span>
+              {order.status === 'WAITING' && '🕞'}
+              {order.status === 'IN_PRODUCTION' && '🧑‍🍳'}
+              {order.status === 'DONE' && '✅'}
+            </span>
+
+            <strong>
+              {order.status === 'WAITING' && 'Fila de espera'}
+              {order.status === 'IN_PRODUCTION' && 'Em preparação'}
+              {order.status === 'DONE' && 'Pronto!'}
+            </strong>
           </div>
         </div>
+
+        <OrderDetails>
+          <strong>Itens</strong>
+          <div className="order-itens">
+            {order.products.map(({ _id, product, quantity }) => (
+              <div className="item" key={_id}>
+                <img
+                  src={`http://localhost:3001/uploads/${product.imagePath}`}
+                  alt={product.name}
+                  width="56"
+                  height="28.51"
+                />
+
+                <span className="quantity">{quantity}x</span>
+
+                <div className="product-details">
+                  <strong>{product.name}</strong>
+                  <span>{formatCurrency(product.price)}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="total">
+            <span>Total</span>
+            <strong>{formatCurrency(total)}</strong>
+          </div>
+        </OrderDetails>
+
+        <Actions>
+          <button type="button" className="primary">
+            <span>🧑‍🍳</span>
+            <strong>Iniciar Produção</strong>
+          </button>
+
+          <button type="button" className="secondary">
+            Candelar Pedido
+          </button>
+        </Actions>
       </ModalBody>
     </Overlay>
   );

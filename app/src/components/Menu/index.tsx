6772fae6cb79cfb933e58e2da2419188ebfac155
@rharison <rharison.abreu@gmail.com>
2022-11-19 15:49:04 -0300

    feat: adjustings in menu component

--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -1,3 +1,44 @@
+import { FlatList } from 'react-native';
+import { products } from '../../mocks/products';
+import {
+  Product,
+  ProductImage,
+  ProductDetails,
+  Separator,
+  AddToCartButton
+} from './styles';
+import { Text } from '../Text';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { PlusCircle } from '../Icons/PlusCircle';
+
 export function Menu() {
-  return null;
+  return (
+    <FlatList
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      data={products}
+      keyExtractor={item => item._id}
+      ItemSeparatorComponent={Separator}
+      renderItem={({ item: product }) => (
+        <Product>
+          <ProductImage
+            source={{
+              uri: `http://192.168.0.116:3001/uploads/${product.imagePath}`,
+            }}
+          />
+          <ProductDetails>
+            <Text weight="600">{product.name}</Text>
+            <Text size={14} color="#666" style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+            <Text size={14} weight="600">{formatCurrency(product.price)}</Text>
+          </ProductDetails>
+
+          <AddToCartButton>
+            <PlusCircle />
+          </AddToCartButton>
+        </Product>
+      )}
+    />
+  );
 }

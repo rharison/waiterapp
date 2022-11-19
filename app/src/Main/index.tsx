e2c2a7c1f7442b50c2309be13a35519ece7ef220
@rharison <rharison.abreu@gmail.com>
2022-11-19 15:48:55 -0300

    feat: adjustings in main component

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -7,9 +7,19 @@ import { Container,
 import { Header } from '../components/Header';
 import { Categories } from '../components/Categories';
 import { Menu } from '../components/Menu';
+import { Button } from '../components/Button';
+import { TableModal } from '../components/TableModal';
+import { useState } from 'react';
 
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState<string | null>(null);
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
   return (
     <>
       <Container>
@@ -26,8 +36,19 @@ export function Main() {
 
       <Footer>
         <FooterContainer>
+          {!selectedTable &&
+            <Button onPress={() => setIsTableModalVisible(true)}>
+              Novo Pedido
+            </Button>
+          }
         </FooterContainer>
       </Footer>
+
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
     </>
   );
 }

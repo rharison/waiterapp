21dcac2373f0d4ce4f72c36cdc4fe687de788816
@rharison <rharison.abreu@gmail.com>
2022-11-19 15:49:14 -0300

    feat: adjustings in categories component

--- app/src/components/Categories/index.tsx
+++ app/src/components/Categories/index.tsx
@@ -1,3 +1,40 @@
+import { categories } from '../../mocks/categories';
+import { FlatList } from 'react-native';
+import { Category, Icon } from './styles';
+import { Text } from '../Text';
+import { useState } from 'react';
+
+
 export function Categories() {
-  return null;
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function handleSelectCategory(categoryId: string) {
+    const category = selectedCategory === categoryId ? '' : categoryId;
+    setSelectedCategory(category);
+  }
+
+  return (
+    <FlatList
+      horizontal
+      contentContainerStyle={{ paddingRight: 24 }}
+      showsHorizontalScrollIndicator={false}
+      data={categories}
+      keyExtractor={category => category._id}
+      renderItem={({ item: category }) => {
+        const isSelected = category._id === selectedCategory;
+        return (
+          <Category onPress={() => handleSelectCategory(category._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>
+                {category.icon}
+              </Text>
+            </Icon>
+            <Text opacity={isSelected ? 1 : 0.5} size={14} weight="600">
+              {category.name}
+            </Text>
+          </Category>
+        );
+      }}
+    />
+  );
 }
